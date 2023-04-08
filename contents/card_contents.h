11a9d944622eca7964bcf5cb322c344cfccf6f7c
Murat <muratcezan@gmail.com>
2023-04-09 01:21:36 +0300

    add contents

---
+++ contents/card_contents.h
@@ -0,0 +1,28 @@
+// Description: Define Card Types
+enum class Types
+{
+    Empty,
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades
+};
+
+// Description: Define Card Names
+enum class Values
+{
+    Empty,
+    Two = 2,
+    There,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Vale,
+    Dam,
+    Rua,
+    As,
+};
